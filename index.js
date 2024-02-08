cb81a086db69875e4060ef2773f7247eab69b645
Stanislav <datura1953@mail.ru>
2024-02-08 19:41:02 +0700

    test

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
+const cardEl = document.querySelectorAll(".card");
 
+cardEl.forEach(function (element) {
+  element.addEventListener("click", addClass);
+});
+
+function addClass() {
+  this.classList.add("active");
+}
